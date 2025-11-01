5992bddd2e6dc92f2de0b40f341cdc19b4f937a5
MuraliKrishna369 <muralikrishnapogiri369@gmail.com>
2025-11-01 23:21:22 +0530

    bugs checked

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Filters from './components/Filters';
-import ProductCard from './components/ProductCard';
 import Body from './components/Body';
 
 function App() {
